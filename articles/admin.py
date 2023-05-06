d66f0fd6ca3b9bd6385afe7625fe80acd77aeeb7
EZ <dw04@ya.ru>
2023-05-06 19:07:20 +0300

    clean rewritten

--- articles/admin.py
+++ articles/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
-# from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 from .models import Article, Scope, Tag
 
 
 class ScopeInlineFormset(BaseInlineFormSet):
-    def formset(self):
-        # for form in self.forms: # В form.cleaned_data будет словарь с
-        # данными # каждой отдельной формы, которые вы можете проверить
-        # form.cleaned_data # вызовом исключения ValidationError можно
-        # указать админке о наличие ошибки # таким образом объект не будет
-        # сохранен, # а пользователю выведется соответствующее сообщение об
-        # ошибке raise ValidationError('Тут всегда ошибка')
-
-        # вызываем базовый код переопределяемого метода
-        return super().formset()
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            data = form.cleaned_data
+            if data.get('is_main') is True:
+                counter += 1
+            else:
+                continue
+        if counter == 0:
+            raise ValidationError('Укажите основную категорию')
+        elif counter > 1:
+            raise ValidationError('Основной может быть только одна категория')
+        return super().clean()
 
 
 class ScopeInline(admin.TabularInline):
